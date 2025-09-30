76ccef5ca07b7c93d9a1484c7e17458d61895162
haya <h2000hhh@gmail.com>
2025-09-30 17:19:47 +0300

    1

---
+++ src/test/java/pages/ABManagerLoginPage.java
@@ -0,0 +1,20 @@
+
+
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+public class ABManagerLoginPage {
+
+    public ABManagerLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//button[.='Bank Manager Login']")
+    public WebElement managerLogin;
+
+    @FindBy(xpath = "//button[.='Customer Login']")
+    public WebElement customerLogin;
+}
+
