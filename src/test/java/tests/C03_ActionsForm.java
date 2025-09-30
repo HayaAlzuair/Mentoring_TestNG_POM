d95a88eae36cab088e842daac28fffefe0a93e06
haya <h2000hhh@gmail.com>
2025-09-30 15:52:05 +0300

    1

---
+++ src/test/java/tests/C03_ActionsForm.java
@@ -0,0 +1,45 @@
+
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ActionsFormPage;
+import pages.AddRecordPage;
+import utilities.Driver;
+
+import java.util.List;
+
+public class C03_ActionsForm {
+/*
+        Go to https://claruswaysda.github.io/ActionsForm.html
+        Fill form and submit
+        Do all actions and assert
+    */
+
+    @Test
+    void C03_ActionsFormTest() {
+        ActionsFormPage formPage = new ActionsFormPage();
+
+//        Go to https://claruswaysda.github.io/ActionsForm.html
+        Driver.getDriver().get("https://claruswaysda.github.io/ActionsForm.html");
+
+//        Fill form and submit
+        formPage
+                .enterName("John")
+                .enterAge("30")
+                .selectDepartment("IT Department")
+                .clickCoding()
+                .clickMale()
+                .clickGenerate()
+                .handleJSAlertPasscode()
+                .clickSubmit();
+
+//        Do all actions and assert
+
+
+
+        Driver.closeDriver();
+
+    }
+    }
+
